311e7a085ba39d6c1cc6312f175d3e3bf781980b
horietakehiro <nashhoward1312@docomo.ne.jp>
2020-10-09 17:30:14 +0900

    reduced manual operations necessary for using aws-sam container

--- lambda/run.sh
+++ lambda/run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [ $# -eq 0 ];
+then
+    echo "specify lambda name"
+    exit
+fi
+LAMBDA=$1
+
+
 docker run \
     -itd \
     --name aws-sam \
     --hostname aws-sam \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ${PWD}/src:/var/opt \
+    -v ${PWD}/config/aws:/root/.aws \
     -e S3_ENDPOINT=http://docker.for.mac.localhost:4566 \
-    -e PYTHONPATH=/var/opt/image-deletion/image_deletion \
+    -e PYTHONPATH=/var/opt/${LAMBDA}/${LAMBDA//-/_} \
     aws-sam:python36
